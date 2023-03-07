a68e68bdc314d876de25fd6cb4059a4eea80b3bb
unknown <vanshhans69@gmail.com>
2023-03-07 14:56:20 +0530

    second commit

--- public/javascripts/showMap.js
+++ public/javascripts/showMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = '<%-process.env.MAPBOX_TOKEN%>';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/light-v10', // style URL
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: [campground[0], campground[1]], // starting position [lng, lat]
     zoom: 8 // starting zoom
 });
