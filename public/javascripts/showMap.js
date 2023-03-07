ae84220f696b4afb35ce4881869a566b010a36c2
unknown <vanshhans69@gmail.com>
2023-03-07 15:03:42 +0530

    third commit

--- public/javascripts/showMap.js
+++ public/javascripts/showMap.js
@@ -1,20 +1,20 @@
-mapboxgl.accessToken = '<%-process.env.MAPBOX_TOKEN%>';
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [campground[0], campground[1]], // starting position [lng, lat]
-    zoom: 8 // starting zoom
-});
-const nav = new mapboxgl.NavigationControl({
-    visualizePitch: true
-});
-map.addControl(nav);
-new mapboxgl.Marker()
-    .setLngLat([campground[0], campground[1]])
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 })
-            .setHTML(
-                // `<p>${window.place.location}</p>`
-            )
-    )
-    .addTo(map)
+// mapboxgl.accessToken = '<%-process.env.MAPBOX_TOKEN%>';
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//     center: [campground[0], campground[1]], // starting position [lng, lat]
+//     zoom: 8 // starting zoom
+// });
+// const nav = new mapboxgl.NavigationControl({
+//     visualizePitch: true
+// });
+// map.addControl(nav);
+// new mapboxgl.Marker()
+//     .setLngLat([campground[0], campground[1]])
+//     .setPopup(
+//         new mapboxgl.Popup({ offset: 25 })
+//             .setHTML(
+//                 // `<p>${window.place.location}</p>`
+//             )
+//     )
+//     .addTo(map)
